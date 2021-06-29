bffdc0a4fc87ebd266de50c576a79cbd090f22f8
Geverson Souza <geversonjosedesouza@gmail.com>
2021-06-28 23:14:03 -0300

    Initial commit

---
+++ src/main/java/br/com/paulistense/mystore/MystoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulistense.mystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/paulistense/mystore/MystoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulistense.mystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
