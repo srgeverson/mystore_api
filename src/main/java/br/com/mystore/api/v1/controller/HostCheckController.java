e9f186e0e8bec12c19a68e769d93a81039c97465
˜GeversonSouza <geversonjosedesouza@gmail.com>
2021-11-14 01:17:03 -0300

    Controlados para verificar qual aplicacao está respondendo.

---
+++ src/main/java/br/com/mystore/api/v1/controller/HostCheckController.java
@@ -0,0 +1,29 @@
+package br.com.mystore.api.v1.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mystore.domain.exception.NegocioException;
+
+@RestController
+@RequestMapping(path = "/v1/hosts", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HostCheckController {
+
+	@GetMapping("/check")
+	public String checkHost() {
+		var host = "";
+		try {
+			host = String.format("IP: %s, Hostname: %s", InetAddress.getLocalHost().getAddress().toString(),
+					InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
+		return host;
+	}
+
+}
