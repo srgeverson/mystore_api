0b4d7334fda8e296b8e421bf09bd21799ebe84ea
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-02-13 10:10:19 -0300

    Comentando a remocao dos gatinho e pprocedures

--- src/main/resources/db/testdata/beforeMigrate.sql
+++ src/main/resources/db/testdata/beforeMigrate.sql
@@ -1,7 +1,7 @@
-USE `mystore`;
+#USE `mystore`;
 
-DROP TRIGGER IF EXISTS `mystore`.`usuarios_AFTER_INSERT`;
+#DROP TRIGGER IF EXISTS `mystore`.`usuarios_AFTER_INSERT`;
 
-DROP procedure IF EXISTS `sp_registra_log`;
+#DROP procedure IF EXISTS `sp_registra_log`;
 
-DROP procedure IF EXISTS `mystore`.`sp_registra_log`;
+#DROP procedure IF EXISTS `mystore`.`sp_registra_log`;
