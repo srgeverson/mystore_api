49a54d03a6aeed703a3f5ec438dce2790652655b
Geverson <geversonjosedesouza@gmail.com>
2022-05-21 11:22:36 -0300

    Adição de consulta ultima atuaização

--- src/main/java/br/com/mystore/domain/repository/EstadoRepository.java
+++ src/main/java/br/com/mystore/domain/repository/EstadoRepository.java
@@ -1,6 +1,9 @@
 package br.com.mystore.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.mystore.domain.model.Estado;
@@ -8,4 +11,13 @@ import br.com.mystore.domain.model.Estado;
 @Repository
 public interface EstadoRepository extends JpaRepository<Estado, Long> {
 
+	@Query("from estados e where e.ativo = :ativo")
+	List<Estado> findByAtivo(Boolean ativo);
+	
+	@Query("from estados e where e.versao > :versao")
+	List<Estado	> findByMaiorVersao(Long versao);
+	
+	@Query("SELECT MAX(e.versao) FROM estados AS e")
+	Long findByUltimaVersao();
+	
 }
